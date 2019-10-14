4ad35472375bc9e5545085c739b7aade391fe380
goodgoodman <gjwnsan9420@gmail.com>
2019-10-15 00:06:50 +0900

    프로젝트 기본 구성 설정 처리

---
+++ src/Application/Application.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CssBaseline from '@material-ui/core/CssBaseline'
+
+const Application = () =>
+  <>
+    <CssBaseline />
+    <div>adsfdfsdfdsafasdfasfsd</div>
+  </>
+
+export default Application
