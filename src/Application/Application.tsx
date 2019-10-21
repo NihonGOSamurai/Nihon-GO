81ef349584db78249a5dd164265a9a7b99513142
goodgoodman <gjwnsan9420@gmail.com>
2019-10-21 23:14:06 +0900

    Styled-Components 처리 추가

--- src/Application/Application.tsx
+++ src/Application/Application.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
+import { StylesProvider } from '@material-ui/styles'
 
 const Application = () =>
-  <>
+  <StylesProvider injectFirst>
     <CssBaseline />
     <div>adsfdfsdfdsafasdfasfsd</div>
-  </>
+  </StylesProvider>
 
 export default Application
