af7b20d38d22ad0c0cea9b3118ea10a0649cf7a4
goodgoodman <gjwnsan9420@gmail.com>
2019-10-26 04:06:37 +0900

    temp

--- src/Application/Application.tsx
+++ src/Application/Application.tsx
@@ -1,11 +1,49 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { StylesProvider } from '@material-ui/styles'
+import Paper, { PaperProps } from '@material-ui/core/Paper'
+import Grid, { GridProps } from '@material-ui/core/Grid'
+import Container from '@material-ui/core/Container'
+import styled from 'styled-components'
+
+const MainContent = styled(Container).attrs({
+  maxWidth: 'lg'
+})<PaperProps>`
+`
+
+const GridContainer = styled(Grid).attrs({
+  container: true,
+  spacing: 2,
+  justify: 'center'
+})<GridProps>`
+`
+
+const GridItem = styled(Grid).attrs({
+  item: true,
+  lg: 6,
+  cols: 2
+})<GridProps>`
+`
 
 const Application = () =>
   <StylesProvider injectFirst>
     <CssBaseline />
-    <div>adsfdfsdfdsafasdfasfsd</div>
+    <GridContainer>
+      <GridItem>
+        <MainContent>
+          <Paper>
+        asdf
+          </Paper>
+        </MainContent>
+      </GridItem>
+      <GridItem>
+        <MainContent>
+          <Paper>
+        asdf
+          </Paper>
+        </MainContent>
+      </GridItem>
+    </GridContainer>
   </StylesProvider>
 
 export default Application
