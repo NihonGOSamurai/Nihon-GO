40d14fb641c0b6f9377883d093cce9a9b90a5b08
goodgoodman <gjwnsan9420@gmail.com>
2019-10-29 01:36:41 +0900

    타이틀 작업

--- src/Application/Application.tsx
+++ src/Application/Application.tsx
@@ -1,49 +1,61 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { StylesProvider } from '@material-ui/styles'
-import Paper, { PaperProps } from '@material-ui/core/Paper'
-import Grid, { GridProps } from '@material-ui/core/Grid'
-import Container from '@material-ui/core/Container'
+import Box, { BoxProps } from '@material-ui/core/Box'
+import TextField, { TextFieldProps } from '@material-ui/core/TextField'
+import Typography, { TypographyProps } from '@material-ui/core/Typography'
+import Container, { ContainerProps } from '@material-ui/core/Container'
+import InputAdornment from '@material-ui/core/InputAdornment'
+import SearchIcon from '@material-ui/icons/Search'
 import styled from 'styled-components'
+import text from '../text'
 
-const MainContent = styled(Container).attrs({
-  maxWidth: 'lg'
-})<PaperProps>`
+const MainContainer = styled(Container).attrs({
+  maxWidth: 'xl'
+})<ContainerProps>`
 `
 
-const GridContainer = styled(Grid).attrs({
-  container: true,
-  spacing: 2,
-  justify: 'center'
-})<GridProps>`
+const TitleBox = styled(Box).attrs({
+  display: 'flex',
+  justifyContent: 'center'
+})<BoxProps>`
 `
 
-const GridItem = styled(Grid).attrs({
-  item: true,
-  lg: 6,
-  cols: 2
-})<GridProps>`
+const Title = styled(Typography).attrs({
+  variant: 'h1',
+  gutterBottom: true
+})<TypographyProps>`
+`
+
+const SearchInputContainer = styled(Container).attrs({
+  maxWidth: 'md'
+})<ContainerProps>`
+`
+
+const SearchInput = styled(TextField).attrs({
+  label: text.search.label,
+  placeholder: text.search.placeholder,
+  fullWidth: true,
+  InputProps: {
+    endAdornment: <InputAdornment position='end'><SearchIcon /></InputAdornment>
+  }
+})<TextFieldProps>`
 `
 
 const Application = () =>
   <StylesProvider injectFirst>
     <CssBaseline />
-    <GridContainer>
-      <GridItem>
-        <MainContent>
-          <Paper>
-        asdf
-          </Paper>
-        </MainContent>
-      </GridItem>
-      <GridItem>
-        <MainContent>
-          <Paper>
-        asdf
-          </Paper>
-        </MainContent>
-      </GridItem>
-    </GridContainer>
+    <MainContainer>
+      <TitleBox>
+        <Title>
+          {text.title}
+        </Title>
+      </TitleBox>
+      <SearchInputContainer>
+        <SearchInput>
+        </SearchInput>
+      </SearchInputContainer>
+    </MainContainer>
   </StylesProvider>
 
 export default Application
---
+++ src/text.ts
@@ -0,0 +1,9 @@
+const text = {
+  title: 'Nihon-GO!',
+  search: {
+    label: '검색',
+    placeholder: '챕터 이름, 태그 등을 입력해주세요.'
+  }
+}
+
+export default text
