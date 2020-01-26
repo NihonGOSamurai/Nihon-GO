02d7e07f8b0468ec521131a58d313da29c232520
goodgoodman <gjwnsan9420@gmail.com>
2020-01-27 02:25:42 +0900

    Temp

--- src/Components/WordCard/WordCard.tsx
+++ src/Components/WordCard/WordCard.tsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from '@material-ui/core/Card'
-import { CardContent, Typography } from '@material-ui/core'
+import { CardContent, Typography, Grid, IconButton, Menu, MenuItem } from '@material-ui/core'
 import { Word } from '../../ChapterItem'
 import FitTextBox from '../FitTextBox'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
 
 export interface WordCardProps {
   word: Word
 }
 
-const WordCard = ({ word: { text, hiraganas } }: WordCardProps) => {
+const WordCard = ({ word: { text, hiraganas, meanings } }: WordCardProps) => {
+  const [menuAnchorElement, setMenuAnchorElement] = useState<null | HTMLElement>(null)
+  const handleClick = ({ currentTarget }: React.MouseEvent<HTMLElement>) => setMenuAnchorElement(currentTarget)
+  const handleClose = () => setMenuAnchorElement(null)
+  const isOpened = Boolean(menuAnchorElement)
   return (
     <Card variant="outlined">
       <CardContent>
-        <FitTextBox component='div' >
-          {text}
-        </FitTextBox>
-        {hiraganas.map((item, i) => <Typography key={i} variant='h6'>{item.value}</Typography>)}
-        {hiraganas.map((item, i) => <Typography key={i} variant='h6'>{item.value}</Typography>)}
+        <Grid container>
+          <Grid item xs={3}>
+            <FitTextBox component='span' >
+              {text}
+            </FitTextBox>
+          </Grid>
+          <Grid item xs={4}>
+            {hiraganas.map((item, i) => <Typography key={i} variant='h6'>{item.value}</Typography>)}
+          </Grid>
+          <Grid item xs={4}>
+            {meanings.map((item, i) => <Typography key={i} variant='h6'>{item.value}</Typography>)}
+          </Grid>
+          <Grid item xs={1}>
+            <IconButton onClick={handleClick}>
+              <MoreVertIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+        <Menu
+          anchorEl={menuAnchorElement}
+          open={isOpened}
+          onClose={handleClose}
+          keepMounted
+        >
+          <MenuItem>
+            Modify
+          </MenuItem>
+          <MenuItem style={{ color: 'red' }}>
+            Delete
+          </MenuItem>
+        </Menu>
       </CardContent>
     </Card>
   )
--- src/Pages/Chapter/Chapter.tsx
+++ src/Pages/Chapter/Chapter.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { useChapterItem } from './useChapter'
 import WordCard from '../../Components/WordCard'
+import { Grid } from '@material-ui/core'
 
 const Chapter = () => {
   const chapterItem = useChapterItem()
   return (
     <>
-      <WordCard word={chapterItem.words[0]} />
+      <Grid container spacing={1}>
+        <Grid item xs={12} md={6}>
+          <WordCard word={chapterItem.words[0]} />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <WordCard word={chapterItem.words[0]} />
+        </Grid>
+
+      </Grid>
     </>
   )
 }
