43256a2b3be30592c411b7a575eb6073d1bcbef9
GoodGoodMan <gjwnsan9420@gmail.com>
2020-01-29 18:37:45 +0900

    change position style of FAB from absolute to fixed

--- src/Components/FAB/FAB.tsx
+++ src/Components/FAB/FAB.tsx
@@ -2,7 +2,7 @@ import { Theme, Fab } from '@material-ui/core'
 import styled, { css } from 'styled-components'
 
 const fabStyle = ({ theme: { spacing } }: { theme: Theme }) => css`
-  position: absolute;
+  position: fixed;
   bottom: ${spacing(2)};
   right: ${spacing(2)};
 `
