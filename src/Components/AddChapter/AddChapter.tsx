f77ee845ab1798ef4e90a70fe963e956c5059a2a
puwon2a62 <puwon2a62@gmail.com>
2020-05-18 01:00:31 +0900

    ../AddChapter.tsx file style changed and  When the button clicked move to chapterPage

--- src/Components/AddChapter/AddChapter.tsx
+++ src/Components/AddChapter/AddChapter.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import AddIcon from '@material-ui/icons/Add'
-import Button from '@material-ui/core/Button'
-const AddChapter = () => {
-  const onClick = () => {
-  }
-  return (
-    <Button onClick={onClick} fullWidth={true} variant="outlined">
-      <AddIcon />
-    </Button>
-  )
-}
+import styled from 'styled-components'
+import ChapterBase, { ChapterBaseProps } from '../ChapterBase'
+
+const ChapterButton = styled(ChapterBase).attrs({
+  variant: 'outlined'
+}) <AddChapterProps>`
+  margin-top: ${({ theme }) => theme.spacing(3)};
+`
+
+export type AddChapterProps = Omit<ChapterBaseProps, 'variant' | 'to'>
+
+const AddChapter = (props: AddChapterProps) =>
+  <ChapterButton to='chapter' {...props}>
+    <AddIcon />
+  </ChapterButton>
 
 export default AddChapter
